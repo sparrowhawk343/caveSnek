9807e5a5463640da1c8b349385747cfd1b239cc3
unknown <victor.jarnberg@gmail.com>
2021-11-03 15:38:38 +0100

    Added LinkedList and interface, empty scripts for planning

---
+++ Assets/Scripts/IDynamicList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ADT
+{
+    public interface IDynamicList<T>
+    {
+        public int Count { get; }
+
+        public int IndexOf(T item);
+
+        public bool Contains(T item);
+
+        public void Add(T item);
+
+        public void Insert(int index, T item);
+
+        public void RemoveAt(int index);
+
+        public bool Remove(T item);
+
+        public void Clear();
+
+        public void CopyTo(T[] target, int index);
+
+        public T this[int index]
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Assets/Scripts/LinkedList.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    public class LinkedList<T> : IDynamicList<T>
+    {
+        private ListNode head;
+        private ListNode tail;
+        private int count;
+
+        public LinkedList()
+        {
+            head = default;
+            tail = default;
+            count = 0;
+        }
+
+        public class ListNode
+        {
+            public T nodeItem;
+            public ListNode nextNode;
+            public ListNode previousNode;
+            public int index = -1;
+
+            public ListNode(T item, ListNode node, ListNode prevNode, int idx)
+            {
+                nodeItem = item;
+                nextNode = node;
+                previousNode = prevNode;
+                index = idx;
+            }
+        }
+
+        public int Count => count;
+
+        public void Add(T item)
+        {
+            ListNode newNode = new ListNode(item, null, tail, count);
+
+            if (count == 0)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.nextNode = newNode;
+                tail = newNode;
+            }
+            count++;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            head = null;
+            tail = null;
+        }
+
+        public bool Contains(T item)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(currentNode.nodeItem, item))
+                {
+                    return true;
+                }
+                currentNode = currentNode.nextNode;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] target, int index)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null && index < target.Length)
+            {
+                target[index] = currentNode.nodeItem;
+                currentNode = currentNode.nextNode;
+                index++;
+            }
+        }
+
+        // rememeber to use the ref keyword when calling this janky bonus method
+        // this method returns a new array with the contents of both the target array and the linked list
+        // index is where the merge is supposed to start inside target
+        public void InsertInArray(ref T[] target, int index)
+        {
+            ListNode currentNode = head;
+            T[] newArray = new T[target.Length + count];
+            int incrementIndex = 0;
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (i == index)
+                {
+                    while (currentNode != null)
+                    {
+                        newArray[i + currentNode.index] = currentNode.nodeItem;
+                        incrementIndex++;
+                        currentNode = currentNode.nextNode;
+                    }
+                }
+                newArray[i + incrementIndex] = target[i];
+            }
+            target = newArray;
+        }
+
+        public void AddRange(T[] target, int index)
+        {
+            for (int i = 0; i < target.Length; i++)
+            {
+                Insert(index + i, target[i]);
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(currentNode.nodeItem, item))
+                {
+                    return currentNode.index;
+                }
+                currentNode = currentNode.nextNode;
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            ListNode currentNode = head;
+            ListNode previousNode = head;
+            ListNode newNode;
+
+            while (currentNode != null)
+            {
+                if (index == currentNode.index)
+                {
+                    newNode = new ListNode(item, currentNode, previousNode, index);
+                    previousNode.nextNode = newNode;
+                    currentNode.previousNode = newNode;
+                    IncreaseIndex(currentNode);
+                    count++;
+                    return;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            ListNode previousNode = head;
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(currentNode.nodeItem, item))
+                {
+                    previousNode.nextNode = currentNode.nextNode;
+                    currentNode.nextNode.previousNode = previousNode;
+                    DecreaseIndex(currentNode.nextNode);
+                    count--;
+                    return true;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+            return false;
+        }
+
+        private void DecreaseIndex(ListNode node)
+        {
+            while (node != null)
+            {
+                node.index--;
+                node = node.nextNode;
+            }
+        }
+
+        private void IncreaseIndex(ListNode node)
+        {
+            while (node != null)
+            {
+                node.index++;
+                node = node.nextNode;
+            }
+        }
+
+        // some array access thing
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= count || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                ListNode currentNode = head;
+                while (currentNode != null)
+                {
+                    if (index == currentNode.index)
+                    {
+                        return currentNode.nodeItem;
+                    }
+                    currentNode = currentNode.nextNode;
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= count || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                ListNode currentNode = head;
+                while (currentNode != null)
+                {
+                    if (index == currentNode.index)
+                    {
+                        currentNode.nodeItem = value;
+                        return;
+                    }
+                    currentNode = currentNode.nextNode;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            ListNode currentNode = head;
+            ListNode previousNode = head;
+
+            while (currentNode != null)
+            {
+                if (index == currentNode.index)
+                {
+                    previousNode.nextNode = currentNode.nextNode;
+                    currentNode.nextNode.previousNode = previousNode;
+                    DecreaseIndex(currentNode.nextNode);
+                    count--;
+                    return;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+        public ListNode GetNode(int index)
+        {
+            ListNode currentNode = head;
+            ListNode previousNode = head;
+
+            while (currentNode != null)
+            {
+                if (index == currentNode.index)
+                {
+                    return currentNode;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
