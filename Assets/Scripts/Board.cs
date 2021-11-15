b43f0f5f8e7c2bd81d9b46acc2e1a6fd79c138cf
unknown <victor.jarnberg@gmail.com>
2021-11-15 16:29:26 +0100

    added cellular automata wall generation. needs tweaking tho

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,36 +7,63 @@ using Cinemachine;
 public class Board : MonoBehaviour
 {
     public static Board instance;
-    
-    private int gridXLength = 20;
-    private int gridYLength = 20;
 
     public HashSet<Vector3> fruitPositions = new HashSet<Vector3>();
 
     public Vector2Int gridSize = new Vector2Int();
+    [Range(0, 100)] public int randomFillPercent = 45;
+    public string seed;
+    public bool useRandomSeed = false;
     public Tile[,] tileGrid;
     public Tile tilePrefab;
     public Wall wallPrefab;
+
+    private int gridXLength = 100;
+    private int gridYLength = 100;
+
+    private Wall[,] walls;
+    private int[,] booleanGrid;
 
     private void Awake()
     {
         gridSize.x = gridXLength;
         gridSize.y = gridYLength;
-        
+
         if (instance == null)
         {
             instance = this;
         }
         else
-        { 
+        {
             Destroy(gameObject);
         }
-        CreateGrid();
+
+        CreateTiles();
+        CreateBooleanGrid();
+        GenerateCaves();
         CreateWalls();
     }
 
+    private void CreateBooleanGrid()
+    {
+        booleanGrid = new int[gridSize.x, gridSize.y];
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
 
-    private void CreateGrid()
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                booleanGrid[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+            }
+        }
+    }
+
+    private void CreateTiles()
     {
         tileGrid = new Tile[gridSize.x, gridSize.y];
         // use this if needed to offset from/to unity's displayed units
@@ -56,7 +83,6 @@ public class Board : MonoBehaviour
 
     private void CreateWalls()
     {
-        Wall[,] walls;
         walls = new Wall[gridSize.x, gridSize.y];
         float wallOffset = 0f;
 
@@ -64,8 +90,7 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < gridSize.y; y++)
             {
-                int wallPercentage = Random.Range(0, 100);
-                if (wallPercentage <= 7)
+                if (booleanGrid[x, y] == 1)
                 {
                     Vector3 wallPosition = new Vector3(x + wallOffset, y + wallOffset, 0);
                     walls[x, y] = Instantiate(wallPrefab, wallPosition, Quaternion.identity);
@@ -77,4 +102,88 @@ public class Board : MonoBehaviour
         }
     }
 
+    // private void CreateWallsOld()
+    // {
+    //     walls = new Wall[gridSize.x, gridSize.y];
+    //     float wallOffset = 0f;
+    //
+    //     for (int x = 0; x < gridSize.x; x++)
+    //     {
+    //         for (int y = 0; y < gridSize.y; y++)
+    //         {
+    //             int wallPercentage = Random.Range(0, 100);
+    //             if (wallPercentage <= 45)
+    //             {
+    //                 Vector3 wallPosition = new Vector3(x + wallOffset, y + wallOffset, 0);
+    //                 walls[x, y] = Instantiate(wallPrefab, wallPosition, Quaternion.identity);
+    //                 walls[x, y].position = new Vector2Int(x, y);
+    //                 walls[x, y].transform.SetParent(gameObject.transform, true);
+    //                 tileGrid[x, y].hasWall = true;
+    //             }
+    //         }
+    //     }
+    // }
+
+    private List<Tile> GetNeighbors(Tile tile)
+    {
+        List<Tile> neighbors = new List<Tile>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+                int checkX = tile.position.x + x;
+                int checkY = tile.position.y + y;
+
+                if (checkX >= 0 && checkX < gridSize.x && checkY >= 0 && checkY < gridSize.y)
+                {
+                    neighbors.Add(tileGrid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+
+    private void GenerateCaves()
+    {
+        int timesToRun = 1;
+        for (int i = 0; i < timesToRun; i++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    Tile currentTile = tileGrid[x, y];
+                    int wallCounter = 0;
+                    List<Tile> neighbors = new List<Tile>();
+                    neighbors = GetNeighbors(currentTile);
+
+                    foreach (Tile tile in neighbors)
+                    {
+                        if (booleanGrid[tile.position.x, tile.position.y] == 1)
+                        {
+                            wallCounter++;
+                        }
+                    }
+
+                    if (booleanGrid[currentTile.position.x, currentTile.position.y] == 1 && wallCounter >= 4)
+                    {
+                        continue;
+                    }
+
+                    if (booleanGrid[currentTile.position.x, currentTile.position.y] == 1 && wallCounter < 4)
+                    {
+                        booleanGrid[currentTile.position.x, currentTile.position.y] = 0;
+                    }
+                }
+            }
+        }
+    }
 }
